1f710aa7031c8be8e8dd39155a6a9176f9d1159c
Yury Kotlyarov <yury.kotlyarov@gmail.com>
2025-02-15 23:49:39 +0300

    python

---
+++ python/estimate_pi.py
@@ -0,0 +1,23 @@
+import math
+import random
+
+in_sector = 0
+count = 100_000_000
+
+for i in range(count):
+  x = random.uniform(0, 1)
+  y = random.uniform(0, 1)
+
+  # найдём длину отрезка
+  z = math.sqrt(x ** 2 + y ** 2)
+
+  if z <= 1:
+    in_sector += 1
+
+# Вероятность попадания в сектор 1/4 окружности с радиусом 1
+P_in_sector = in_sector / count
+
+print("Оценка pi = ", 4 * P_in_sector)
+
+
+
