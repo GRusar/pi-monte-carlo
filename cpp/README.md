fa8c20b7b11d587a15ba534448cb953ea751c400
Yury Kotlyarov <yury.kotlyarov@gmail.com>
2025-02-15 23:39:19 +0300

    cpp

---
+++ cpp/README.md
@@ -0,0 +1,3 @@
+```sh
+clang++ -O3 -march=native estimate_pi.cpp -o estimate_pi -std=c++17 
+```
---
+++ cpp/estimate_pi.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <random>
+#include <cmath>
+
+int main() {
+    const long count = 100'000'000;
+    int inSector = 0;
+
+    // Инициализация 64-битного генератора случайных чисел
+    std::random_device rd;
+    std::mt19937_64 gen(rd());
+    std::uniform_real_distribution<double> dist(0.0, 1.0);
+
+    for (long i = 0; i < count; ++i) {
+        double x = dist(gen);
+        double y = dist(gen);
+        if (std::sqrt(x * x + y * y) <= 1.0) {
+            ++inSector;
+        }
+    }
+
+    double pi = 4.0 * static_cast<double>(inSector) / static_cast<double>(count);
+    std::cout << "pi: " << pi << std::endl;
+
+    return 0;
+}
