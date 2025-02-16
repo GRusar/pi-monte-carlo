4ca322585608d942c84cacd04d528f5f2ab8215d
Yury Kotlyarov <yury.kotlyarov@gmail.com>
2025-02-16 09:50:54 +0300

    cpp: var declaration outside of the cycle

--- cpp/estimate_pi.cpp
+++ cpp/estimate_pi.cpp
@@ -3,17 +3,18 @@
 #include <cmath>
 
 int main() {
-    const long count = 100'000'000;
+    const long count = 1'000'000'000;
     int inSector = 0;
 
     // Инициализация 64-битного генератора случайных чисел
     std::random_device rd;
     std::mt19937_64 gen(rd());
     std::uniform_real_distribution<double> dist(0.0, 1.0);
+    double x, y;
 
     for (long i = 0; i < count; ++i) {
-        double x = dist(gen);
-        double y = dist(gen);
+        x = dist(gen);
+        y = dist(gen);
         if (std::sqrt(x * x + y * y) <= 1.0) {
             ++inSector;
         }
